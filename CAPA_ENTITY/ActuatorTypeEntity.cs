6f841bfa25a88ccae82a441c9ba9d72d3af2c655
Adrian Víquez Martínez <adrian.viquez@icumed.com>
2026-04-17 15:53:16 -0600

    Actualizacion de atributos e identidades

--- CAPA_ENTITY/ActuatorTypeEntity.cs
+++ CAPA_ENTITY/ActuatorTypeEntity.cs
@@ -6,7 +6,11 @@ namespace CAPA_ENTITY
 {
     internal class ActuatorTypeEntity
     {
-        public int? ActuatorID { get; set; }
+        public int ActuatorID { get; set; }
         public string? ActuatorType { get; set; }
+        public DateTime DateCreation { get; set; }
+        public DateTime? DateModification { get; set; }
+        public string? CreatedBy { get; set; }
+        public string? ModifiedBy { get; set; }
     }
 }
