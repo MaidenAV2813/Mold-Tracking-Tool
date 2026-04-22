a4e3de877edd3ecf2a4a9583e4a70db364808e8f
Adrian Víquez Martínez <adrian.viquez@icumed.com>
2026-04-22 09:57:49 -0600

    Actualizacion de plantilla

---
+++ Tracking Tool System/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tracking_Tool_System.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
