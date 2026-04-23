cfb4949a5071ced302a87fc4621117088d79a157
Adrian Víquez Martínez <adrian.viquez@icumed.com>
2026-04-22 20:48:16 -0600

    Actualizacion

---
+++ Tracking Tool System/Pages/Shared/_navbar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Tracking_Tool_System.Pages.Shared
+{
+    public class _navbarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
